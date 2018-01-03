26c78e805288f982e65275a576adf93278f855eb
cichol <cichol@live.cn>
2018-01-03 14:54:18 -0500

    add test for linear_solver

--- lib/chemical_equation_balancer/linear_solver.rb
+++ lib/chemical_equation_balancer/linear_solver.rb
@@ -1,6 +1,8 @@
 class LinearSolver
+  NoSolutionError = Class.new(RuntimeError)
+
   def initialize mat
-      @mat = mat
+    @mat = mat
   end
 
   # returns an 2-D array where each element is a Rational
@@ -8,7 +10,8 @@ class LinearSolver
     lead = 0
     rows = ary.size
     cols = ary[0].size
-    rary = convert_to(ary, :to_r)  # use rational arithmetic
+    rary = ary.map{|row| row.map(&:to_r)}
+
     catch :done  do
       rows.times do |r|
         throw :done  if cols <= lead
@@ -38,11 +41,35 @@ class LinearSolver
     rary
   end
 
-  # type should be one of :to_s, :to_i, :to_f, :to_r
-  def convert_to(ary, type)
-    ary.each_with_object([]) do |row, new|
-      new << row.collect {|elem| elem.send(type)}
+  def reduced_row_echelon_form1(ary)
+    rary = ary.map{|row| row.map(&:to_r)}
+
+    rary.size.times do |processing|
+      (processing...rary.size).each do |index|
+        rary[index] = normalize_line(rary[index], processing)
+      end
+      p [processing, rary]
+      if rary[processing][processing] == 0
+        non_zero = rary[processing..-1].find{|x| x[processing] != 0}
+        rary[processing] = subtract_line(non_zero, rary[processing]) if non_zero
+      end
+
+      (processing+1...rary.size).each do |index|
+        rary[index] = subtract_line(rary[index], rary[processing]) if rary[index][processing] != 0
+      end
+      p rary
     end
+    rary
+
+  end
+
+  def normalize_line line, index
+    return line if line[index] == 0
+    line.map{|x| x / line[index]}
+  end
+
+  def subtract_line line1, line2
+    line1.zip(line2).map{|a, b| a - b}
   end
 
   class Rational
@@ -59,10 +86,10 @@ class LinearSolver
   end
 
   def solve
-    result_mat=reduced_row_echelon_form(@mat)
-    cols=@mat[0].size
-    rows=@mat.size
-    rank=0
+    result_mat = reduced_row_echelon_form(@mat)
+    cols = @mat[0].size
+    rows = @mat.size
+    rank = 0
     for i in 0..[cols-1,rows-1].min
       if result_mat[i][i]!=0 then
         rank+=1
@@ -72,7 +99,7 @@ class LinearSolver
     #齐次线性方程组在满秩的情况下只有0解，对于本应用没有意义，认为是无解
     #此外对于方程式配平而言只存在自由变量是一个的情况
     if rank!=(cols-1) then
-      raise 'No solution.'
+      raise NoSolutionError, 'No solution.'
     end
 
     free_var_count=cols-rank
@@ -106,3 +133,11 @@ class LinearSolver
   end
 
 end
+
+#mat = [[0, 0, -2, 0], [3, 4, -12, -1], [0, 2, 0, -2], [0, 1, -3, 0]]
+mat = [[2, 3, 5, 6],
+[4, 1, 4, 5],
+[1, 2, 3, 4],
+[3, 6, 7, 9]]
+solver = LinearSolver.new(mat)
+p solver.reduced_row_echelon_form1(mat)

fac980c64bffa96e47928824a294c6f959ddd4c1
cichol <cichol@live.cn>
2018-01-03 13:24:27 -0500

    refactor to_matrix

--- lib/chemical_equation_balancer/parser_result_to_matrix_helper.rb
+++ lib/chemical_equation_balancer/parser_result_to_matrix_helper.rb
@@ -1,46 +1,21 @@
 class ParserResultToMatrixHelper
-  def initialize left,right
+  def initialize left, right
     @left=left
     @right=right
   end
 
   def to_matrix
-    elem_hash=Hash.new
-    elem_index=0;
+    elem_hash = @left.inject(:+).map(&:first).uniq.map.with_index.to_h
 
-    @left.each do |i|
-      i.each do |j|
-        if elem_hash[j[0]]==nil then
-          elem_hash[j[0]]=elem_index
-          elem_index+=1
-        end
+    molecules = @left + @right.map{|molecule| molecule.map{|elem, num| [elem, -num]}}
+    mat = elem_hash.map{ [0] * molecules.size }
+
+    molecules.each_with_index do |molecule, index|
+      molecule.each do |element|
+        mat[elem_hash[element[0]]][index] += element[1]
       end
     end
 
-    elem_count=elem_hash.size
-    molecule_count=@left.size+@right.size
-
-    mat=Array.new(elem_count)
-
-    for i in 0..elem_count-1
-      mat[i]=Array.new(molecule_count,0)
-    end
-
-    molecule_index=0
-
-    @left.each do |i|
-      i.each do |j|
-        mat[elem_hash[j[0]]][molecule_index]+=j[1]
-        end
-        molecule_index+=1
-    end
-
-    @right.each do |i|
-      i.each do |j|
-        mat[elem_hash[j[0]]][molecule_index]-=j[1]
-        end
-        molecule_index+=1
-    end
     mat
   end
 end
